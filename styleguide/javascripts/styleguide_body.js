f03a4147657170a8d78a4a8c0244b6a88b641622
Neson <neson@dex.tw>
2015-06-21 02:56:45 +0800

    collapsed HTML code block in styleguide

--- styleguide/javascripts/styleguide_body.js
+++ styleguide/javascripts/styleguide_body.js
@@ -14,4 +14,19 @@ $(document).on('ready page:load', function () {
       if (!window.index) { window.location = '../' };
     }
   });
+
+  // Collapse html block on code examples
+  $('.styleguide-example').append('<div class="styleguide-example-show-code-toggle">Show Code</div>');
+  $('.styleguide-example-show-code-toggle').click(function (e) {
+    $toggle = $(e.target);
+    $htmlBlock = $toggle.parent().find('.styleguide-html');
+    $htmlBlock.collapse('toggle');
+    if ($toggle.html() == 'Show Code') {
+      $toggle.html('Hide Code');
+    } else {
+      $toggle.html('Show Code');
+    }
+  });
+
+  $('.styleguide-html').addClass('collapse');
 });
