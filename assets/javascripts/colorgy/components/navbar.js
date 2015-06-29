b6b81fde0acf9e4e9d7170c20fe778d77c09da7b
ChengHsuanLiu <chenghsuango@gmail.com>
2015-06-29 20:49:16 +0800

    js broken issue fixed

--- assets/javascripts/colorgy/components/navbar.js
+++ assets/javascripts/colorgy/components/navbar.js
@@ -1,14 +1,10 @@
 // jQuery navbar scroll effects
 $(window).scroll(function() {
-  if ($('.navbar.auto-expanded').offset().top > 50 && $('.navbar.auto-expanded').offset().top) {
+  if ($(window).scrollTop() > 50 ) {
     $('.navbar.auto-expanded').removeClass('expanded');
-  } else {
-    $('.navbar.auto-expanded').addClass('expanded');
-  }
-
-  if ($('.navbar.auto-transparent').offset().top > 50 && $('.navbar.auto-transparent').offset().top) {
     $('.navbar.auto-transparent').removeClass('transparent');
   } else {
+    $('.navbar.auto-expanded').addClass('expanded');
     $('.navbar.auto-transparent').addClass('transparent');
   }
 });
