bf9244f49f505ee378821ad011fa10393a128e55
ChengHsuanLiu <chenghsuango@gmail.com>
2015-06-29 20:41:02 +0800

    js broken issue fixed

--- assets/javascripts/colorgy/components/navbar.js
+++ assets/javascripts/colorgy/components/navbar.js
@@ -1,14 +1,16 @@
 // jQuery navbar scroll effects
 $(window).scroll(function() {
-  if ($('.navbar.auto-expanded').offset().top > 50 && $('.navbar.auto-expanded').offset().top) {
-    $('.navbar.auto-expanded').removeClass('expanded');
-  } else {
-    $('.navbar.auto-expanded').addClass('expanded');
-  }
+  if($('.navbar.auto-expanded')){
+    if ($('.navbar.auto-expanded').offset().top > 50 ) {
+      $('.navbar.auto-expanded').removeClass('expanded');
+    } else {
+      $('.navbar.auto-expanded').addClass('expanded');
+    }
 
-  if ($('.navbar.auto-transparent').offset().top > 50 && $('.navbar.auto-transparent').offset().top) {
-    $('.navbar.auto-transparent').removeClass('transparent');
-  } else {
-    $('.navbar.auto-transparent').addClass('transparent');
+    if ($('.navbar.auto-transparent').offset().top > 50 ) {
+      $('.navbar.auto-transparent').removeClass('transparent');
+    } else {
+      $('.navbar.auto-transparent').addClass('transparent');
+    }
   }
 });
